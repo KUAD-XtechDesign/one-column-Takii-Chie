04f6a308225502f7accd9ee23073e5ea787e839b
Takii_Chie <68629404+Takii-Chie@users.noreply.github.com>
2020-10-05 05:14:46 +0900

    トップまで戻るボタンを作成

--- work/js/script.js
+++ work/js/script.js
@@ -13,4 +13,13 @@ $(function(){
       $(this).parent(".content").slideUp("slow");
     })
   
+    $(window).scroll(function () {
+     var now = $(window).scrollTop();
+     if (now > 200) {
+       $('.pagetop').fadeIn("slow");
+     } else {
+       $('.pagetop').fadeOut('slow');
+     }
+    });
+
   });
